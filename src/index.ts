fa6361a7af6ca0c6989a1346dac8bf6d7e1abc83
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-04-13 18:35:10 -0300

    Criação das rotas

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-import express, {Request, Response, NextFunction} from 'express';
+
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
 
+// Config da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send( {foo: 'dalionhendituy'});
-});
 
+// Config de rotas
+app.use(statusRoute);
+app.use(usersRoute);
+
+
+
+// Inicialização do servidor
 app.listen(3000, () => {
     console.log('Application running on port 3000.');
 });
