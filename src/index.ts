e3f58bf2b4675e14d7249611d156c92c9e840d95
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-04-13 17:29:56 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import express, {Request, Response, NextFunction} from 'express';
 
+const app = express();
+
+
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send( {foo: 'dalionhendituy'});
+});
+
+app.listen(3000, () => {
+    console.log('Application running on port 3000.');
+});
